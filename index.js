790a41ed8f16e27c2823eabe6b93e0cdbd57c1a4
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 21:06:36 +0300

    Fix for a cleaner code

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const { create } = require('domain');
 const express = require('express');
-const { createConnection } = require('./db/db.js');
 const { Database } = require('./db/db.js');
+
 const app = express();
 require('dotenv').config();
 
+//Process env destructure
+const { PORT } = process.env;
+
 const db = new Database();
 // db.createConnection();
+
+app.listen(PORT, () => {
+  console.log(`Server up and running! -> http://localhost:${PORT}`);
+});
